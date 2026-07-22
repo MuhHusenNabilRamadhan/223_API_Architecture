d57b8608a15e88bfe1d756bb163d25bba8200509
MuhhusenNabilRamadhan <secondchoice230906@gmail.com>
2026-07-22 14:46:45 +0700

    express akan berjalan di port 3000

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
-const express = require('express');
-const connectToDatabase = require('./config/db');
+const express = require("express");
+const connectToDatabase = require("./config/db");
+
+const app = express();
+const PORT = 3000;
